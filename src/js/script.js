ba5ad497cf2e5804108c65755b1b0e16cb2b3a11
Manuel Rauber <manuel.rauber@thinktecture.com>
2019-02-06 16:52:28 +0100

    Support image gallery & upstream stuff

--- src/js/script.js
+++ src/js/script.js
@@ -107,6 +107,7 @@ jQuery(function ($) {
         grid();
         ajaxLinkClass();
         video();
+        gallery();
         highlight();
         currentMenuFix();
         integrateGhostHunter();
@@ -144,6 +145,22 @@ jQuery(function ($) {
     }
 
     ajaxLinkClass();
+
+    /* ==========================================================================
+	   Gallery
+	   ========================================================================== */
+
+    function gallery() {
+        var images = document.querySelectorAll('.kg-gallery-image img');
+        images.forEach(function (image) {
+            var container = image.closest('.kg-gallery-image');
+            var width = image.attributes.width.value;
+            var height = image.attributes.height.value;
+            var ratio = width / height;
+            container.style.flex = ratio + ' 1 0%';
+        });
+    }
+    gallery();
 
     /* ==========================================================================
      Ajax Loading
